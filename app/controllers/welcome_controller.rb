fe5205f56f9f07f33edce86c458ff211bc44e3a6
john <piloto2810@gmail.com>
2019-12-19 14:28:57 -0300

    todos los controladores, vistas, y modelos

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+  end
+  
+  def create
+    @article = Article.new(article_params)
+   
+    @article.save
+    redirect_to @article
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  devise_for :models
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  root 'welcome#index'
+end
