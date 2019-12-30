609dee7dfa19d3a4a420663047762a8013b25eec
john <piloto2810@gmail.com>
2019-12-30 14:16:24 -0300

    [mostrar-autor-del-articulo]
    se agrega mostrar autor de articilo
    
    this closes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_12_27_183148) do
+=======
+ActiveRecord::Schema.define(version: 2019_12_26_171036) do
+>>>>>>> Stashed changes
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
