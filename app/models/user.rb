96bf2296c20b9d7aea6c4202c7a4582b49ff724e
john <piloto2810@gmail.com>
2020-01-03 18:05:35 -0300

    [cambiar el formato de fecha y hora]
    - se cambian los formatos de fecha y hora
    - se agrega metodo para establecer nombre o email en los comentarios y articulos
    
    this closes #14

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def name_or_email
+   self.name.present? && self.last_name.present? ? "#{self.name} #{self.last_name}".capitalize : self.email.capitalize
+  end
 end
