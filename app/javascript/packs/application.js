3cb14a68209bf0a64fe4da987e3be48304ef5099
john <piloto2810@gmail.com>
2020-01-17 10:40:23 -0300

    [feature/bootstrap4]
    se corrigen fallas en bootstrap4
    
    this closes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,18 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "bootstrap";
-import "/home/john/blogsito/app/assets/stylesheets/application.scss";
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 
 
-require("@rails/ujs").start()
+// require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("jquery3")
-require("popper")
-require("bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -23,5 +21,5 @@ require("bootstrap")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
+// require("trix")
 require("@rails/actiontext")
